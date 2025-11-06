8d06f37880098d90adf4e45ea02747896318d78e
Kevin Glory Prasetyo 71220958 <117893068+Kevin-Glory-Prasetyo@users.noreply.github.com>
2025-11-06 11:13:57 +0700

    Forget Password
    Forget Password Problem kode kadaluarsa

--- js/profile.js
+++ js/profile.js
@@ -1,39 +1,3 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    const res = await axios.get("http://localhost:5000/auth/checkLogin", {
-      withCredentials: true
-    });
-
-    const data = res.data;
-
-    if (res.status === 401 || res.status === 403) {
-      window.location.href = "login.html";
-      return;
-    }
-
-    if (data.user.role !== "user") {
-      window.location.href = "dashboard_admin.html";
-      return;
-    }
-
-    if (res.status === 200) {
-      
-      const namaPengguna = document.getElementById("nama-pengguna");
-      const emailPengguna = document.getElementById("email-pengguna");
-  
-      namaPengguna.textContent = `${data.user.first_name} ${data.user.last_name}`;
-      emailPengguna.textContent = data.user.email;
-    } else{
-      alert(data.message || "Terjadi kesalahan");
-    }
-
-
-  } catch (err) {
-    console.error("Fetch gagal:", err);
-    window.location.href = "login.html";
-  }
-});
-
 // Logout
 const logoutBtn = document.getElementById("logoutBtn");
 if (logoutBtn) {
@@ -101,33 +65,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
-
-
-// Scroll Coming Soon books
-const bookSlider = document.getElementById("bookSlider");
-const leftBtn = document.querySelector(".nav-btn.left");
-const rightBtn = document.querySelector(".nav-btn.right");
-
-leftBtn.addEventListener("click", () => {
-  bookSlider.scrollBy({ left: -300, behavior: 'smooth' });
-});
-
-rightBtn.addEventListener("click", () => {
-  bookSlider.scrollBy({ left: 300, behavior: 'smooth' });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Tombol lihat sekarang
-  const seeNowBtn = document.getElementById("seeNowBtn");
-
-  if (seeNowBtn) {
-    seeNowBtn.addEventListener("click", () => {
-      console.log("Tombol Lihat Sekarang diklik!"); // Debug
-      window.location.href = "/Library-Book-Front-End/html/kategori.html";
-    });
-  } else {
-    console.warn("Tombol #seeNowBtn tidak ditemukan di halaman ini.");
-  }
-});
-
-
